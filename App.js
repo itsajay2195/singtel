a5e3bbd1629f00edcd8eeb1bfdff2e930b458426
itsajay2195 <ajaykumar.95@yahoo.com>
2022-02-28 18:49:58 +0530

    completion of the header component

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light"/>
+      <Header/>
+      <Text style={{color:'white'}}>Open up App.js to start working on your app!</Text>
+     
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#2C3333',
+    color:'white',
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { COLORS, SIZES } from '../constants/theme'
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+
+            <TouchableOpacity >
+                <Text style={styles.buttonRestartText}>Restart</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity >
+                <Text style={styles.stepsText}>STEPS: <Text style={{color:COLORS.primaryBlue}}>0</Text></Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row', 
+        height: SIZES.height / 10, 
+        // backgroundColor: 'red', 
+        alignItems: 'center', 
+        justifyContent: 'space-between', 
+        padding: SIZES.padding
+    },
+    buttonRestartText:{
+        color: COLORS.primaryBlue,
+        fontSize: SIZES.h2,
+    },
+    stepsText:{
+        color: COLORS.white,
+        fontSize: SIZES.h1,
+    }
+})

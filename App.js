23fab42a5e47a0bb4098dc75e0947976516a4411
itsajay2195 <ajaykumar.95@yahoo.com>
2022-03-01 18:53:26 +0530

    cosmetic changes in and around the card

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Card from './components/Card';
 
 
 export default function App() {
+  const data = [
+    { id: 3, value: 1 },
+    { id: 5, value: 2},
+    { id: 2, value: 3 },
+    { id: 4, value: 4 },
+    { id: 6, value: 5 },
+    { id: 1, value: 6 },
+    { id: 7, value: 6 },
+    { id: 12, value: 5 },
+    { id: 11, value: 4 },
+    { id: 8, value: 3 },
+    { id: 9, value: 2 },
+    { id: 10, value: 1 },
+
+]
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light"/>
       <Header/>
       <View style={{flex:1,justifyContent:'center',alignItems:'center', overflow: 'hidden'}}>
-        <Card/>
+        <FlatList
+            data={data}
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={3}
+            renderItem={({ item }) => <Card item={item.value}/>}
+        />
       </View>
      
     </SafeAreaView>
--- components/Card.js
+++ components/Card.js
@@ -1,83 +1,56 @@
-import { Animated, FlatList, StyleSheet, Text, TouchableOpacity,View, Button} from 'react-native'
-import React, { useRef,useState } from 'react'
+import { Animated, FlatList, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native'
+import React, { useRef, useState } from 'react'
 import { SIZES, COLORS } from '../constants/theme'
 
 
+const value = SIZES.height - SIZES.height / 10
 
-const data = [
-    { id: 3, value:1 },
-    { id: 5, value: 2},
-    { id: 2, value: 3 },
-    { id: 4, value: 4 },
-    { id: 6, value: 5 },
-    { id: 1, value: 6 },
-    { id: 7, value: 7 },
-    { id: 12, value: 8 },
-    { id: 11, value: 9 },
-    { id: 8, value: 10 },
-    { id: 9, value: 11 },
-    { id: 10, value: 12 },
+const Card = ({item}) => {
+    const animate = useRef(new Animated.Value(0))
+    const [isFlipped, setIsFlipped] = useState(false)
 
-]
-
-
-const value = SIZES.height - SIZES.height / 10 
-
-const Card = () => {
-    const animate =useRef(new Animated.Value(0))
-    const [isFlipped,setIsFlipped] = useState(false)
-    
-    const handleFlip = ()=>{
-        Animated.timing(animate.current,{
-            duration:1000,
-            toValue:isFlipped ? 0 : 180,
-            useNativeDriver:true,
-        }).start(()=>{
+    const handleFlip = () => {
+        Animated.timing(animate.current, {
+            duration: 300,
+            toValue: isFlipped ? 0 : 180,
+            useNativeDriver: true,
+        }).start(() => {
             setIsFlipped(!isFlipped)
         })
         console.log('im clicked')
     }
 
     const interpolateFront = animate.current.interpolate({
-        inputRange:[0,180],
-        outputRange:['0deg','180deg']
+        inputRange: [0, 180],
+        outputRange: ['0deg', '180deg']
     });
 
     const interpolateBack = animate.current.interpolate({
-        inputRange:[0,180],
-        outputRange:['180deg','360deg']
+        inputRange: [0, 180],
+        outputRange: ['180deg', '360deg']
     });
 
 
     return (
-            <Animated.FlatList
-                // scrollEnabled={true}
-                data={data}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => (
-                    <>
-                        <Animated.View style={[{transform:[{rotateY:interpolateFront}]},styles.hidden]}>
-                            <TouchableOpacity style={styles.item} onPress={()=>handleFlip()}>
-                                <Text>?</Text>
-                            </TouchableOpacity>
-                         </Animated.View>
-                        {/* <Animated.View style={[styles.back,styles.hidden,{transform:[{rotateX:interpolateBack}]}]}>
-                            <TouchableOpacity style={styles.item} onPress={()=>handleFlip()}>
-                                <Text >{item.value}</Text>
-                            </TouchableOpacity>
-                        </Animated.View> */}
-                    </>
+        <View>
+            <>
+                <Animated.View style={[{ transform: [{ rotateY: interpolateFront }] }, styles.hidden]}>
+                    <TouchableOpacity style={styles.item} onPress={() => handleFlip()}>
+                        <Text style={{fontSize:SIZES.h3}}>Front</Text>
+                    </TouchableOpacity>
+                </Animated.View>
+                <Animated.View style={[styles.back, styles.hidden, { transform: [{ rotateX: interpolateBack }] }]}>
+                    <TouchableOpacity style={[styles.item,{backgroundColor:COLORS.white}]} onPress={() => handleFlip()}>
+                        <Text style={{fontSize:SIZES.h2}} >{item}</Text>
+                    </TouchableOpacity>
+                </Animated.View>
+            </>
+        </View>
 
-                 
-                   
-                )}
-            numColumns={3}
-            />
 
-     
 
-          
-      
+
+
 
     )
 }
@@ -86,19 +59,19 @@ export default Card
 
 const styles = StyleSheet.create({
     item: {
-        height: (value - (10*20))/4, //columns
-        width: (SIZES.width -105)/3, //rows
-        backgroundColor: COLORS.primaryBlue,
+        height: (value - (10 * 20)) / 4, //columns
+        width: (SIZES.width - 105) / 3, //rows
+        backgroundColor:COLORS.primaryBlue,
         borderWidth: 1,
-        borderRadius: 10,marginVertical:5,marginHorizontal:5,
+        borderRadius: 10, marginVertical: 5, marginHorizontal: 5,
         justifyContent: 'center',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    hidden:{
-        // backfaceVisibility:'hidden'
+    hidden: {
+        backfaceVisibility: 'hidden'
     },
-    back:{
-        position:'absolute',
-        top:0
+    back: {
+        position: 'absolute',
+        top: 0,
     }
 })
